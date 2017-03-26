e155dcfa0085d1b78bd849d29c564e783beef9be
electricman94 <ryan.pennell94@hotmail.com>
2017-03-25 21:51:50 -0700

    cleaned up a bit

--- Subset.py
+++
@@ -1,5 +0,0 @@
-class Subset():
-    def __init__(self, *funct):
-        for i in funct:
-            if (callable(i)):
-                self.__dict__[i.__name__] = i
--- Util.py
+++ Util.py
@@ -2,6 +2,22 @@ from threading import Thread
 from multiprocessing import Event, Queue
 import sys
 
+from pprint import pprint
+
+def pretty_print(obj, level = 0):
+    if (hasattr(obj, '__iter__') and not isinstance(obj, str)):
+        for i in obj:
+            pretty_print(i, level + 1)
+    else:
+        print((" " * 2 * level) + str(obj))
+
+def error(token, *args):
+    string = str(token) + ": "
+    for i in args:
+        string += str(i) + " "
+
+    print string
+
 def probe(*args, **kwargs):
     delim = ", "
     event = delim.join(['%s' % i for i in args])
